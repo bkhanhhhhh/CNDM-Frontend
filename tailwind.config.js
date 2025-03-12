45440e41011a8fbdf788839dab328c118978eba2
tpbkkkkk <145079106+bkhanhhhhh@users.noreply.github.com>
2025-03-12 14:26:43 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'green': {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+        },
+        'purple': {
+          50: '#faf5ff',
+          100: '#f3e8ff',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
